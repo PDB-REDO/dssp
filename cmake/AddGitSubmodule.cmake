2baf702e3790cd147ff6c587f356011e9f572710
Maarten L. Hekkelman <maarten@hekkelman.com>
2022-10-30 19:51:13 +0100

    fix for meta project

--- cmake/AddGitSubmodule.cmake
+++ cmake/AddGitSubmodule.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.19)
+cmake_minimum_required(VERSION 3.16..3.19)
 
 function(add_git_submodule dir)
 	# add a Git submodule directory to CMake, assuming the
@@ -11,9 +11,14 @@ function(add_git_submodule dir)
 	find_package(Git REQUIRED)
 
 	if(NOT EXISTS ${dir}/CMakeLists.txt)
-		execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
-			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-			COMMAND_ERROR_IS_FATAL ANY)
+		if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.19)
+			execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
+				WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+				COMMAND_ERROR_IS_FATAL ANY)
+		else()
+			execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
+				WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+		endif()
 	endif()
 
 	set(ENABLE_TESTING OFF)
