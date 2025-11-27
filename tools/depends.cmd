a92752fff74ee0ca584b6a13e1240790179c6ac3
Maarten L. Hekkelman <maarten@hekkelman.com>
2025-11-27 16:09:55 +0100

    Try again

--- tools/depends.cmd
+++ tools/depends.cmd
@@ -1,6 +1,5 @@
 @ECHO OFF
 SET ZLIB_VERSION=1.3
-SET PCRE2_VERSION=10.45
 
 IF NOT EXIST build_ci\libs (
   MKDIR build_ci\libs
@@ -20,21 +19,6 @@ CD zlib-%ZLIB_VERSION%
 cmake -B build || EXIT /b 1
 cmake --build build --target ALL_BUILD --config Release || EXIT /b 1
 cmake --build build --target RUN_TESTS --config Release || EXIT /b 1
-cmake --build build --target INSTALL --config Release || EXIT /b 1
-
-@REM Install PCRE2
-IF NOT EXIST pcre2-%PCRE2_VERSION%.zip (
-  ECHO Downloading https://github.com/PCRE2Project/pcre2/releases/download/pcre2-%PCRE2_VERSION%/pcre2-%PCRE2_VERSION%.zip
-  curl -L -o pcre2-%PCRE2_VERSION%.zip https://github.com/PCRE2Project/pcre2/releases/download/pcre2-%PCRE2_VERSION%/pcre2-%PCRE2_VERSION%.zip || EXIT /b 1
-)
-IF NOT EXIST pcre2-%PCRE2_VERSION% (
-  ECHO Unpacking pcre2-%PCRE2_VERSION%.zip
-  C:\windows\system32\tar.exe -x -f pcre2-%PCRE2_VERSION%.zip || EXIT /b 1
-)
-CD pcre2-%PCRE2_VERSION%
-cmake -B build -DPCRE2_BUILD_PCRE2GREP=OFF || EXIT /b 1
-cmake --build build --target ALL_BUILD --config Release || EXIT /b 1
-@REM cmake --build build --target RUN_TESTS --config Release || EXIT /b 1
 cmake --build build --target INSTALL --config Release || EXIT /b 1
 
 @EXIT /b 0
